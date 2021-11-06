1d5ad6d51d20c5a9539c8617f36a1201c1e9b57e
Rebecca Paolucci <repaolucci@gmail.com>
2021-11-05 23:25:57 -0500

    added api.py

---
+++ api.py
@@ -0,0 +1,23 @@
+from flask import Flask, jsonify, request
+from flask_restful import Resource, Api, reqparse
+import json, os
+
+app = Flask(__name__)
+api = Api(app)
+
+
+class BookCoverService(Resource):
+
+    def get(self):
+        json_data = request.get_json(force=True)
+        ISBN = json_data["ISBN"]
+        image = "https://covers.openlibrary.org/b/isbn/" + ISBN + "-M" + ".jpg"
+        return_dict = {"Cover": image}
+        return return_dict
+
+
+api.add_resource(BookCoverService, '/book')
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 4000))
+    app.run(port=port, debug=True)
